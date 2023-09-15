fdae6d68573fd8125c40fe092725e42d82f8a5a2
shwetasidhwani <shweta.sidhwani@gmail.com>
2023-09-16 01:23:19 +0530

    About Us pg

--- Aboutus/aboutus.js
+++ Aboutus/aboutus.js
@@ -6,13 +6,95 @@ var typed = new Typed('#title', {
 });
 
 var a1 = document.getElementById('v-git')
-a.addEventListener('click',function(){
+a1.addEventListener('click',function(){
   const redirectURL='https://github.com/Vaishz';
-  window.location.href = redirectURL;
+  window.open(redirectURL,'_blank');
 })
 
-var b1 = document.getElementById('v-git')
-a.addEventListener('click',function(){
-  const redirectURL='';
-  window.location.href = redirectURL;
+var b1 = document.getElementById('v-mail')
+b1.addEventListener('click',function(){
+  function mailTo()
+{
+window.location.href = "mailto:poti.vaishnavi21@gmail.com";
+}
+ 
 })
+
+var c1 = document.getElementById('v-link')
+c1.addEventListener('click',function(){
+  const redirectURL='https://www.linkedin.com/in/vaishnavi-v-poti-9b33a225a/';
+  window.open(redirectURL,'_blank');
+})
+
+
+
+
+var a2 = document.getElementById('a-git')
+a2.addEventListener('click',function(){
+  const redirectURL='https://github.com/AshmitSrivastava';
+  window.open(redirectURL,'_blank');
+})
+
+var b2 = document.getElementById('a-mail')
+b2.addEventListener('click',function(){
+  function mailTo()
+{
+window.location.href = "mailto:ashmitsrivastava1607@gmail.com";
+}
+ 
+})
+
+var c2 = document.getElementById('a-link')
+c2.addEventListener('click',function(){
+  const redirectURL='https://www.linkedin.com/in/ashmit-srivastava-4443ba21a';
+  window.open(redirectURL,'_blank');
+})
+
+
+
+
+var a3 = document.getElementById('d-git')
+a3.addEventListener('click',function(){
+  const redirectURL='https://github.com/Dhruvt0310';
+  window.open(redirectURL,'_blank');
+})
+
+var b3 = document.getElementById('d-mail')
+b3.addEventListener('click',function(){
+  function mailTo()
+{
+window.location.href = "mailto:: taterdhruv0310@gmail.com";
+}
+ 
+})
+
+var c4 = document.getElementById('d-link')
+c4.addEventListener('click',function(){
+  const redirectURL='';
+  window.open(redirectURL,'_blank');
+})
+
+
+
+
+var a4 = document.getElementById('s-git')
+a4.addEventListener('click',function(){
+  const redirectURL='https://github.com/shwetasidhwani';
+  window.open(redirectURL,'_blank');
+})
+
+var b4 = document.getElementById('s-mail')
+b4.addEventListener('click',function(){
+  function mailTo()
+{
+window.location.href = "mailto:shweta.sidhwani@gmail.com";
+}
+ 
+})
+
+var c1 = document.getElementById('s-link')
+c1.addEventListener('click',function(){
+  const redirectURL='https://www.linkedin.com/in/shweta-sidhwani-b66153287/';
+  window.open(redirectURL,'_blank');
+})
+
