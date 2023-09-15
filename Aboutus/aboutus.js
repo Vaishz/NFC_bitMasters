0a723e5988c4ce2d6e0509c8da7597239db6576d
shwetasidhwani <shweta.sidhwani@gmail.com>
2023-09-16 00:22:54 +0530

    Shweta push

--- Aboutus/aboutus.js
+++ Aboutus/aboutus.js
@@ -4,3 +4,15 @@ var typed = new Typed('#title', {
   backspeed: 100,
   loop: true
 });
+
+var a1 = document.getElementById('v-git')
+a.addEventListener('click',function(){
+  const redirectURL='https://github.com/Vaishz';
+  window.location.href = redirectURL;
+})
+
+var b1 = document.getElementById('v-git')
+a.addEventListener('click',function(){
+  const redirectURL='';
+  window.location.href = redirectURL;
+})
