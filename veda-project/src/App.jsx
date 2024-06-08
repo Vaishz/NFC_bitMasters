b98c5712843ce71352c3d90ffb979bc23295dbc2
yepvaishz <poti.vaishnavi21@gmail.com>
2024-06-08 18:06:41 +0530

    -

--- veda-project/src/App.jsx
+++ veda-project/src/App.jsx
@@ -5,10 +5,10 @@ import Footer from "./Footer/Footer";
 import Home from "./Home/Home";
 import AboutUs from './Aboutus/AboutUs'
 import Alternatives from "./Alternatives/Alternatives"
-// import Diseases from './body-types/Diseases';
-// import KaphaBodyType from './body-types/KaphaBodyType';
-// import Vata from './body-types/VataBodyType';
-// import Pitta from './body-types/PittaBodyType';
+import Diseases from './body-types/Diseases';
+import KaphaBodyType from './body-types/KaphaBodyType';
+import Vata from './body-types/VataBodyType';
+import Pitta from './body-types/PittaBodyType';
 // import DigestiveSystem from "./Digestive/DigestiveSystem";
 // import Books from "./Books/Books";
 import Products from "./Products/Products";
