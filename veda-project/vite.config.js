8e0a9f16b9d0f3739240e8ecfa810f8820fd4c34
yepvaishz <poti.vaishnavi21@gmail.com>
2024-06-19 04:54:48 +0530

    Update base path for GitHub Pages deployment

--- veda-project/vite.config.js
+++ veda-project/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Vaishz/NFC_bitMasters/veda-project', 
   plugins: [react()],
-})
+});
