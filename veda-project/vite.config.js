a5134b17cb2f06fc874de04ebeb975bc77a023d2
yepvaishz <poti.vaishnavi21@gmail.com>
2024-06-19 05:06:58 +0530

    cicd

--- veda-project/vite.config.js
+++ veda-project/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Vaishz/NFC_bitMasters/veda-project', 
+  base: '/NFC_bitMasters/veda-project', 
   plugins: [react()],
 });
